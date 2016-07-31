c8e03c7c3230dc32569433c491a01b02b8c42db8
Yang <y.he0802@gmail.com>
2016-07-31 00:34:58 -0500

    Fix the issue that application.R does not run
    It had three problems: (1) a typo in function name (2) a typo in
    function argument and (3) some data sets are not loaded. This commit
    fixes these issues but does not generate results in .tex file.

--- R/application.R
+++ R/application.R
@@ -30,12 +30,12 @@ original <- function(dta, bw) {
 }
 
 robust <- function(dta) {
-  bws <- rdbandwidth_2014(dta$y, dta$x, kernel = "uni")$bws
+  bws <- rdbwselect_2014(dta$y, dta$x, kernel = "uni")$bws
   cct <- rdrobust(dta$y, dta$x, kernel = "uni", h = bws[1], b = bws[2])
   t1  <- cct$coef[3]
   ci1 <- cct$ci[3, ]
 
-  boot <- rdboot(dta$t, dta$x, 1 - level, N.bc, wild_bootstrap, N.ci,
+  boot <- rdboot(dta$y, dta$x, 1 - level, N.bc, wild_bootstrap, N.ci,
     type = "basic", kernel = "uniform")
   t2 <- boot[1]
   ci2 <- boot[2:3]
@@ -48,10 +48,20 @@ robust <- function(dta) {
 spending <- matrix(0, nrow = 10, ncol = 6)
 colnames(spending) <- c("Effect", "Lower CI", "Upper CI", "P-value", "h", "b")
 
+hsspend_per_kid_68 <- census2[ , c("hsspend_per_kid_68", "povrate60")]
+hsspend_per_kid_68 <- hsspend_per_kid_68[complete.cases(hsspend_per_kid_68), ]
+colnames(hsspend_per_kid_68) <- c("y", "x")
+hsspend_per_kid_68$x <- hsspend_per_kid_68$x - cutoff
+
 spending[1, ] <- original(hsspend_per_kid_68, 9)
 spending[2, ] <- original(hsspend_per_kid_68, 18)
 spending[3, ] <- original(hsspend_per_kid_68, 36)
 spending[4:5, ] <- robust(hsspend_per_kid_68)
+
+hsspend_per_kid_72 <- census2[ , c("hsspend_per_kid_72", "povrate60")]
+hsspend_per_kid_72 <- hsspend_per_kid_72[complete.cases(hsspend_per_kid_72), ]
+colnames(hsspend_per_kid_72) <- c("y", "x")
+hsspend_per_kid_72$x <- hsspend_per_kid_72$x - cutoff
 
 spending[6, ] <- original(hsspend_per_kid_72, 9)
 spending[7, ] <- original(hsspend_per_kid_72, 18)
@@ -63,6 +73,11 @@ spending[9:10, ] <- robust(hsspend_per_kid_72)
 mortality <- matrix(0, nrow = 5, ncol = 6)
 colnames(mortality) <- c("Effect", "Lower CI", "Upper CI", "P-value", "h", "b")
 
+age5_9_sum2 <- census3[ , c("age5_9_sum2", "povrate60")]
+age5_9_sum2 <- age5_9_sum2[complete.cases(age5_9_sum2), ]
+colnames(age5_9_sum2) <- c("y", "x")
+age5_9_sum2$x <- age5_9_sum2$x - cutoff
+
 mortality[1, ] <- original(age5_9_sum2, 9)
 mortality[2, ] <- original(age5_9_sum2, 18)
 mortality[3, ] <- original(age5_9_sum2, 36)
@@ -73,8 +88,18 @@ mortality[4:5, ] <- robust(age5_9_sum2)
 education <- matrix(0, nrow = 6, ncol = 6)
 colnames(education) <- c("Effect", "Lower CI", "Upper CI", "P-value", "h", "b")
 
+hsplus18_24 <- census_1990[ , c("hsplus18_24", "povrate60")]
+hsplus18_24 <- hsplus18_24[complete.cases(hsplus18_24), ]
+colnames(hsplus18_24) <- c("y", "x")
+hsplus18_24$x <- hsplus18_24$x - cutoff
+
 education[1, ] <- original(hsplus18_24, 7)
 education[2:3, ] <- robust(hsplus18_24)
+
+some_clg18_24 <- census_1990[ , c("some_clg18_24", "povrate60")]
+some_clg18_24 <- some_clg18_24[complete.cases(some_clg18_24), ]
+colnames(some_clg18_24) <- c("y", "x")
+some_clg18_24$x <- some_clg18_24$x - cutoff
 
 education[4, ] <- original(some_clg18_24, 7)
 education[5:6, ] <- robust(some_clg18_24)
